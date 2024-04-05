4ac1b7818fe62d5c02d427cb3f087b6de5e9cbd6
max-taylor <39660195+max-taylor@users.noreply.github.com>
2024-04-06 08:10:19 +1000

    first commit

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set envvar for dotfiles dir
+export DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Kitty
+rm -rf $HOME/.config/kitty
+mkdir -p $HOME/.config/kitty
+ln -snf $DOTFILES/kitty/* $HOME/.config/kitty
+
+# ZSH
+
+# # Install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
+
+# Tmux
+# Detect if already installed and skip installing tpm if so
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+ln -sf $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
+
+# Git
+ln -sf $DOTFILES/git/.gitconfig $HOME/.gitconfig
+
+# Nvim
+rm -rf $HOME/.config/nvim
+mkdir -p $HOME/.config/nvim
+ln -snf $DOTFILES/nvim/* $HOME/.config/nvim
+
+# # MacOS applications
+# if [[ $OSTYPE == "darwin"* ]]; then
+#   . "$DOTFILES/install/brew-cask.sh"
+# fi
