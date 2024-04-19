d268e8a980be0a1cb7d06dc1e491ebf07fed91dd
max-taylor <39660195+max-taylor@users.noreply.github.com>
2024-04-19 17:14:13 +1000

    refactor: small updates

--- nvim/lua/custom/plugins/local-dev.lua
+++ nvim/lua/custom/plugins/local-dev.lua
@@ -1,5 +1,11 @@
 return {
   {
+    'MunifTanjim/nui.nvim',
+    config = function()
+      require('custom.extensions.git').setup()
+    end,
+  },
+  {
     dir = '~/Documents/Code/Neovim/plugin/',
     name = 'task-manager',
     dependencies = { 'MunifTanjim/nui.nvim', 'kikito/middleclass' },
