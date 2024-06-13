bbc03f4b63056d679e6f18638bdc4b769cc67820
max-taylor <39660195+max-taylor@users.noreply.github.com>
2024-06-14 07:42:37 +1000

    feat: working through it

--- nvim/lua/plugins/edgy-nvim.lua
+++ nvim/lua/plugins/edgy-nvim.lua
@@ -1,3 +1,49 @@
+local function is_within_cwd(filepath)
+	-- Get the current working directory
+	local cwd = vim.fn.getcwd()
+	-- Normalize the filepath
+	local normalized_path = vim.fn.fnamemodify(filepath, ":p")
+	-- Check if the normalized_path starts with the cwd
+	return string.sub(normalized_path, 1, #cwd) == cwd
+end
+
+local function oldfilesOpen()
+	-- Function to open the old files list
+	-- local bufnr = vim.api.nvim_create_buf(false, true) -- Create a new buffer
+	vim.api.nvim_command("enew") -- Open a new empty buffer
+	local bufnr = vim.api.nvim_get_current_buf()
+	local oldfiles = vim.v.oldfiles
+	local results = {}
+
+	for _, file in ipairs(oldfiles) do
+		if is_within_cwd(file) then
+			table.insert(results, file)
+			vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { file })
+		end
+
+		if #results == 5 then
+			break
+		end
+	end
+
+	-- Set the buffer's file type to 'oldfiles' (for consistency)
+	vim.bo[bufnr].filetype = "oldfiles"
+	vim.bo[bufnr].buftype = "nofile"
+	vim.bo[bufnr].swapfile = false
+	vim.bo[bufnr].modifiable = false
+	-- Add keybinding on enter to open the hovered file
+	vim.keymap.set("n", "<CR>", function()
+		local columnPosition = vim.api.nvim_win_get_cursor(0)[1]
+		local filepath = results[columnPosition]
+		vim.cmd("edit " .. vim.fn.fnameescape(filepath))
+
+		-- vim.cmd("buffer " .. vim.fn.fnameescape(filepath))
+		-- vim.bo.vim.api.nvim_win_set_buf(0, vim.api.nvim_get_current_buf())
+		-- print(item)
+		-- vim.api.nvim_command("edit " .. oldfiles[columnPosition])
+	end, { noremap = true, silent = true, buffer = bufnr })
+end
+
 return {
 	{
 		"folke/edgy.nvim",
@@ -65,26 +111,7 @@ return {
 					title = "Old Files",
 					size = { height = 0.4 },
 					pinned = true, -- Keep the view always shown in the edgebar
-					open = function()
-						-- -- Function to open the old files list
-						-- local bufnr = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-						-- print(bufnr)
-						-- Function to open the old files list
-						vim.api.nvim_command("enew") -- Open a new empty buffer
-						local bufnr = vim.api.nvim_get_current_buf()
-						local oldfiles = vim.v.oldfiles
-
-						-- Iterate over oldfiles up to 5 and add them to the buffer
-						for i = 1, 5 do
-							vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { oldfiles[i] })
-						end
-
-						-- Set the buffer's file type to 'oldfiles' (for consistency)
-						vim.bo[bufnr].filetype = "oldfiles"
-						vim.bo[bufnr].buftype = "nofile"
-						vim.bo[bufnr].swapfile = false
-						vim.bo[bufnr].modifiable = false
-					end,
+					open = oldfilesOpen,
 					-- wo = {
 					-- 	-- Window-specific options
 					-- 	number = false, -- Disable line numbers
@@ -92,8 +119,6 @@ return {
 					-- 	cursorline = true, -- Highlight the current line
 					-- },
 					filter = function(buf, win)
-						print("logging num")
-						print(buf)
 						return vim.bo[buf].filetype == "oldfiles"
 					end,
 				},
