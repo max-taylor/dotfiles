ff3c2aefb8d8cae4b0f9491476514821421d4479
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-05-26 08:18:25 +1000

    feat: splits partially working

--- setup.sh
+++ setup.sh
@@ -4,9 +4,16 @@
 export DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Kitty
-rm -rf $HOME/.config/kitty
-mkdir -p $HOME/.config/kitty
-ln -snf $DOTFILES/kitty/* $HOME/.config/kitty
+# rm -rf $HOME/.config/kitty
+# mkdir -p $HOME/.config/kitty
+# ln -snf $DOTFILES/kitty/* $HOME/.config/kitty
+
+# TODO: Symlink the entire directory so we can create local files
+rm -rf $HOME/.config/wezterm
+mkdir -p $HOME/.config/wezterm
+# Symlink the wezterm config file
+ln -snf $DOTFILES/wezterm/* $HOME/.config/wezterm
+# ln -sf $DOTFILES/wezterm/.wezterm.lua $HOME/.wezterm.lua
 
 # ZSH
 
