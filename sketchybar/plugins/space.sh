e1d2155ffc2412b4ed9c4e4c84acdc4c9886ffcf
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-10-20 16:57:28 +1000

    fix

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ "$SELECTED" = "true" ]; then
+  sketchybar -m --set $NAME background.color=0xff81a1c1
+else
+  sketchybar -m --set $NAME background.color=0xff57627A
+fi
