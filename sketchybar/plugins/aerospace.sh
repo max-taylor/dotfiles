72f0d847e7b78ddd178af2a8d814ebf8fb513426
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-10-21 09:15:32 +1000

    working

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on background.color=0xff81a1c1
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.drawing=off background.color=0xff57627A
 fi
