ff2c27b7d22219ebefd5f7f4387fabe7528b908f
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-11-30 13:26:32 +1000

    fixed minor issues

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,9 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import LazyVim extras
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,19 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    -- Disable LazyVim's default rename binding
-    keys[#keys + 1] = { "<leader>cr", false }
-
-    -- Add custom keybindings
-    keys[#keys + 1] = { "<leader>rn", vim.lsp.buf.rename, desc = "Rename" }
-    keys[#keys + 1] = {
-      "gv",
-      function()
-        require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
-      end,
-      desc = "Goto Definition (vsplit)",
-    }
-  end,
+  opts = {
+    servers = {
+      -- Apply these keymaps to all LSP servers
+      ["*"] = {
+        keys = {
+          {
+            "gv",
+            function()
+              require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
+            end,
+            desc = "Goto Definition (vsplit)",
+          },
+        },
+      },
+    },
+  },
 }
